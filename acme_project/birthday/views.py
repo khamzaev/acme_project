f2f54222a3a4e41050fb94c64d4706260b5a416a
Khamzaev <khamzaew.j@gmail.com>
2024-12-27 09:21:05 +0300

    edit_post

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 
-def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+def edit_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(request.POST or None, instance=instance)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
